4c796353889089c082786576815cc8327a8071c7
Martin Milius <martin.milius@gmail.com>
2019-08-14 02:24:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
- new_hash = {:railroads => {:rent_in_dollars => 4}}
+ new_hash = {:railroads => {:rent_in_dollars => {:one, :two, :three, :four}}}
 end
 
 def monopoly_with_fourth_tier
